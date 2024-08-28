2ef581a865bae0a9903961e208e6701b70c57014
ryugyungyoon <alslehs@gmail.com>
2024-08-28 15:47:32 +0900

    exspring 프로젝트 생성

---
+++ src/main/java/com/rc/exspring/ExspringApplication.java
@@ -0,0 +1,13 @@
+package com.rc.exspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rc/exspring/ExspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.exspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
